7a990913da45a063e691792451d8d8da3908e79b
robin <rfussell17@gmail.com>
2024-11-21 12:52:39 -0800

    remove fontswap

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,10 +17,6 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <link
-          rel="stylesheet"
-          href="https://api.fontshare.com/css?f%5B%5D=switzer@400,500,600,700&amp;display=swap"
-        />
-        <link
           rel="alternate"
           type="application/rss+xml"
           title="The Radiant Blog"
